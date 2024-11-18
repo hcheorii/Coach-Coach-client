ed42cbe0f0e992b518cd72ea6f9958ec5a17bceb
lee-hyeon-cheol <leehc991028@gmail.com>
2024-11-18 21:39:14 +0900

    Refactor: url수정 및 UI 수정

--- src/components/common/Header/LogoHeader.tsx
+++ src/components/common/Header/LogoHeader.tsx
@@ -1,22 +1,40 @@
 import logoPath from "@/assets/images/logo.svg";
 import { useFetchAuth } from "@/hooks/useFetchAuth";
-import { Link } from "react-router-dom";
+import { useFetchNotifications } from "@/hooks/queries/useNotification";
+import { Link, useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
+import Icon from "@/components/Icon/Icon";
+import NotificationBadge from "@/components/badge/NotificationBadge";
 
 const LogoHeader = () => {
   const { data } = useFetchAuth();
+  const { data: notificationData } = useFetchNotifications();
+  const navigate = useNavigate();
+
+  const onClickAlarm = () => {
+    navigate("/notification");
+  };
+
   return (
     <LogoHeaderStyle>
       <LogoLink to="/home">
         <img src={logoPath} alt="Logo" />
       </LogoLink>
-      {data?.nickname && (
-        <h2>
-          <span>안녕하세요! </span>
-          {data?.nickname}
-          <span>님</span>
-        </h2>
-      )}
+      <UserSection>
+        {data?.nickname && (
+          <h2>
+            <span>안녕하세요! </span>
+            {data?.nickname}
+            <span>님</span>
+          </h2>
+        )}
+        <NotificationButton onClick={onClickAlarm}>
+          <Icon name="alarm" size="25px" color="gray3" />
+          {notificationData && notificationData.length > 0 && (
+            <NotificationBadge count={notificationData.length} />
+          )}
+        </NotificationButton>
+      </UserSection>
     </LogoHeaderStyle>
   );
 };
@@ -47,6 +65,20 @@ const LogoLink = styled(Link)`
     height: 100%;
     object-fit: contain;
   }
+`;
+
+const UserSection = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`;
+
+const NotificationButton = styled.button`
+  position: relative;
+  background: none;
+  border: none;
+  padding: 0;
+  cursor: pointer;
 `;
 
 export default LogoHeader;
